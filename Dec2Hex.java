ffa4d94def0a1c7d22e471067a37de12e8844a69
Ubuntu <ubuntu@ip-172-31-16-113.ec2.internal>
2021-11-05 13:44:56 +0000

    fouth

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,7 +7,18 @@ class Dec2Hex
 
     public static void main(String args[])
     {
+	if (args.lenght > 0)
+	{
+	try
+	{
         Arg1 = Integer.parseInt(args[0]);
+	}
+	catch (NumberFormantException e)
+	{
+	System.err.println("Argument" + args[0] + " must be an integer");
+	System.exit(1);
+	}
+	}
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
@@ -25,4 +36,4 @@ class Dec2Hex
 
     }
 }
-//hello
+//helloword
